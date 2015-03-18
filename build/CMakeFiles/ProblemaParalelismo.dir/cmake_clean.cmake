3165377a69a9aefdac427963c1e34807e8e59141
Matheus Kautzmann <kautzmann5@gmail.com>
2015-03-18 14:24:00 -0300

    Cria Makefile com CMake garantindo o uso de C++11 e já faz o build para OSX Intel

---
+++ build/CMakeFiles/ProblemaParalelismo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProblemaParalelismo.dir/main.cpp.o"
+  "bin/ProblemaParalelismo.pdb"
+  "bin/ProblemaParalelismo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProblemaParalelismo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
